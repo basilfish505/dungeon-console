8a33804e77cf9210b9db1acd6d5473a547621651
basilfish505 <basilfish505@gmail.com>
2025-03-23 09:31:03 -0700

    added player properties

--- dungeon_crawler.py
+++ dungeon_crawler.py
@@ -17,27 +17,32 @@ class Player:
         self.pos = position
         self.level = 1
         self.xp = 0
-        # New HP properties
-        self.mhp = random.randint(10, 20)  # Max HP between 10-20
-        self.hp = self.mhp                  # Current HP starts at max
-        # Other stats
+        # HP/MP properties
+        self.mhp = random.randint(10, 20)
+        self.hp = self.mhp
+        self.mmp = 0
+        self.mp = 0
+        # Stats
         self.str = random.randint(1, 10)
         self.int = random.randint(1, 10)
         self.wis = random.randint(1, 10)
         self.chr = random.randint(1, 10)
         self.dex = random.randint(1, 10)
+        self.agi = random.randint(1, 10)  # New AGI stat
     
     def to_dict(self):
         return {
             'id': self.id,
             'level': self.level,
             'xp': self.xp,
-            'hp': f"{self.hp}/{self.mhp}",  # Format as "current/max"
+            'hp': f"{self.hp}/{self.mhp}",
+            'mp': f"{self.mp}/{self.mmp}",
             'str': self.str,
             'int': self.int,
             'wis': self.wis,
             'chr': self.chr,
-            'dex': self.dex
+            'dex': self.dex,
+            'agi': self.agi
         }
 
     def move(self, direction):
